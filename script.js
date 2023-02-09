fcbb75300b8ccf39ad61a428f3ac88c533972183
m4tiolli <gabrielmatiollif@gmail.com>
2023-02-09 17:34:25 -0300

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+  const date = new Date();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const numberDay = date.getDay();
+  const arrayDay = new Array(
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+  );
+  const day = arrayDay[numberDay];
+  document.getElementById(
+    "clock"
+  ).innerHTML = `<p class="day">${day}</p><p class="time">${hour}:${minutes}:${seconds}</p>`;
+}, 1);

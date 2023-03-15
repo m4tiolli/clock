3e477103979a12e516cd3681046d43e3682808a8
Gabriel Matiolli <97938132+m4tiolli@users.noreply.github.com>
2023-03-15 17:07:31 +0000

    .

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 setInterval(() => {
   const date = new Date();
-  const hour = date.getHours() < 10 ? "0" + date.getHours(): date.getHours();
-  const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes();
-  const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds(): date.getSeconds();
+  const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
   const numberDay = date.getDay();
   const arrayDay = new Array(
     "Domingo",
